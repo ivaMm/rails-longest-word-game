96374fb9b6b6bbfd93e74c2fa53ea838063fb541
iva mudronja <iva.pilizota@gmail.com>
2020-06-19 18:49:21 +0200

    improved

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,28 +4,29 @@ require 'json'
 class GamesController < ApplicationController
   def new
     @letters = ("A".."Z").to_a.sample(10)
+    @start_time = Time.now
   end
 
   def score
   	# raise
+    date_arr = params[:start_time].split(/-|\s|:/)
+    tz = date_arr[-1].insert(3, ":")
+    nums = date_arr[0...-1].map { |n| n.to_i }
+    start_time = Time.new(nums[0], nums[1], nums[2], nums[3], nums[4], nums[5], tz)
+    end_time = Time.now
+    @elapsed_t = (end_time - start_time).ceil(2)
+    @score = 0
+
   	@word = params[:attempt].upcase
   	@grid = params[:grid]
-
-    if attempt_in_grid?(@word, @grid)
-      if attempt_in_dictionary?(@word)
-        @msg = "Congratulation! #{@word} is a valid English word."
-      else
-        @msg = "Sorry but #{@word} is not an English word." 
-      end
-    else
-      @msg = "Sorry, but #{@word} can’t be built out of #{@grid}."
-    end
+    @in_grid = attempt_in_grid?(@word, @grid)
+    @in_dictionary = attempt_in_dictionary?(@word)
   end
 
   private
 
   def attempt_in_grid?(word, grid)
-  	word.chars.sort.all? { |letter| word.count(letter) <= grid.count(letter) }
+  	word.chars.all? { |letter| word.count(letter) <= grid.count(letter) }
   end
 
   def attempt_in_dictionary?(word)
