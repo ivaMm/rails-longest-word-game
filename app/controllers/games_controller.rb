182129b38eb476fc108814f574ca280c3eb2cbe8
iva mudronja <iva.pilizota@gmail.com>
2021-01-06 19:16:00 +0100

    improved

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
+
 require 'open-uri'
 require 'json'
 
 class GamesController < ApplicationController
+  VOWELS = %w[A E I O U Y].freeze
+
   def new
-    @letters = ("A".."Z").to_a.sample(10)
+    @letters = VOWELS.sample(5)
+    @letters += (('A'..'Z').to_a - VOWELS).sample(5)
+    @letters.shuffle!
     @start_time = Time.now
   end
 
   def score
-  	# raise
-    date_arr = params[:start_time].split(/-|\s|:/)
-    tz = date_arr[-1].insert(3, ":")
-    nums = date_arr[0...-1].map { |n| n.to_i }
-    start_time = Time.new(nums[0], nums[1], nums[2], nums[3], nums[4], nums[5], tz)
+    start_time = params[:start_time].to_time
     end_time = Time.now
     @elapsed_t = (end_time - start_time).ceil(2)
     @score = 0
 
-  	@word = params[:attempt].upcase
-  	@grid = params[:grid]
+    @word = params[:attempt].upcase
+    @grid = params[:grid]
     @in_grid = attempt_in_grid?(@word, @grid)
     @in_dictionary = attempt_in_dictionary?(@word)
   end
@@ -26,13 +28,13 @@ class GamesController < ApplicationController
   private
 
   def attempt_in_grid?(word, grid)
-  	word.chars.all? { |letter| word.count(letter) <= grid.count(letter) }
+    word.chars.all? { |letter| word.count(letter) <= grid.count(letter) }
   end
 
   def attempt_in_dictionary?(word)
     url = "https://wagon-dictionary.herokuapp.com/#{word}"
     dictionary = open(url).read
     word = JSON.parse(dictionary)
-    word["found"]
+    word['found']
   end
 end
